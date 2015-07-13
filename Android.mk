36e590a58e00b2be82ed72cb0cb09009dac4b765
Alberto96 <albertoguanti@gmail.com>
2015-07-13 23:18:17 +0200

    android_packages_apps_SuperSU: initialize repo

---
+++ Android.mk
@@ -0,0 +1,58 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= SuperSU_apk
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= app/SuperSU/Superuser.apk
+LOCAL_MODULE_PATH	:= $(PRODUCT_OUT)/system/app/SuperSU/
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= SuperSU_installed
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/.installed_su_daemon
+LOCAL_MODULE_PATH	:= $(PRODUCT_OUT)/system/etc/
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= SuperSU_install-rec
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/install-recovery.sh
+LOCAL_MODULE_PATH	:= $(PRODUCT_OUT)/system/etc/
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= SuperSU_init
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/init.d/99SuperSUDaemon
+LOCAL_MODULE_PATH	:= $(PRODUCT_OUT)/system/etc/init.d/
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= SuperSU_libsupol
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= lib/libsupol.so
+LOCAL_MODULE_PATH	:= $(PRODUCT_OUT)/system/lib/
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE				:= SuperSU_subinary
+LOCAL_MODULE_TAGS			:= optional eng
+LOCAL_MODULE_CLASS			:= ETC
+LOCAL_SRC_FILES				:= xbin/su
+LOCAL_MODULE_PATH			:= $(PRODUCT_OUT)/system/xbin/
+LOCAL_OVERRIDES_PACKAGES 	:= su
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= SuperSU_supolicy
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= xbin/supolicy
+LOCAL_MODULE_PATH	:= $(PRODUCT_OUT)/system/xbin/
+include $(BUILD_PREBUILT)
